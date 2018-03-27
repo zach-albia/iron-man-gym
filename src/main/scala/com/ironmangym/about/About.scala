48ea3c907f02a8e9d606a0aef21f86caf12790a7
KD <kentdunlagaras@gmail.com>
2018-03-27 13:09:22 +0400

    Added temp About page

--- src/main/scala/com/ironmangym/about/About.scala
+++ src/main/scala/com/ironmangym/about/About.scala
@@ -1,5 +1,6 @@
 package com.ironmangym.about
 
+import com.pangwarta.sjrmui.{ Card, CardContent, CardMedia, Typography }
 import japgolly.scalajs.react._
 import japgolly.scalajs.react.vdom.html_<^._
 
@@ -9,11 +10,27 @@ object About {
       .renderStatic(<.div(
         ^.margin := "0 auto",
         ^.marginTop := 40.px,
-        ^.maxWidth := 900.px,
+        ^.maxWidth := 960.px,
         ^.paddingLeft := 24.px,
-        ^.paddingRight := 24.px
-      ))
+        ^.paddingRight := 24.px,
+        Card(
+          className = "classes.card"
+        )()(
+          Typography(variant = Typography.Variant.headline)()(" IRON MAN GYM"),
+          CardMedia(
+            className = "classes.media",
+            image     = "target/scala-2.12/classes/workoutChalk.jpg",
 
+          )()(
+            Typography(variant = Typography.Variant.caption)()("Lemme check if this is working")
+          )
+
+        //<.img(
+        //^.src := "target/scala-2.12/classes/workoutChalk.jpg",
+        //,
+        //^.height := 48.px, ^.width := 48.px, ^.marginRight := 12.px
+        )
+      ))
       .build
   def apply() = component()
 
